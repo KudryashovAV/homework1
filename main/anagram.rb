0ef32b674be8df7dec4f317212421f8d2c5e4b9f
KudryashovAV <hekpomamohtehok@gmail.com>
2014-05-25 03:35:29 +0400

    during

--- main/anagram.rb
+++ main/anagram.rb
@@ -2,6 +2,6 @@ module Anagram
   extend self
 
   def anagrams(str)
-    []
+    str.delete(",.!?;:").downcase.split.each{|x, y| x.chars.sort.join == y.chars.sort.join}
   end
 end
--- main/word_count.rb
+++ main/word_count.rb
@@ -2,6 +2,6 @@ module Words
   extend self
 
   def count(str)
-    {}
+    str.delete(",.!?;:").downcase.split.group_by {|i| i = i}
   end
 end
