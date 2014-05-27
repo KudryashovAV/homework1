540725ecb23072b04f3aad2f455026d9e577e55e
Kudryashov <hekpomamohtehok@gmail.com>
2014-05-27 09:34:25 +0300

    2.1 finish

--- main/rock_paper_scisors.rb
+++ main/rock_paper_scisors.rb
@@ -6,30 +6,30 @@ class RockPaperScissors
   def self.winner(move1, move2)
   	def move1
       move1 = [name1, stroke1]
-        raise NoSuchStrategyError if name1 == nil || stroke1 != (r.to_s.downcase || p.to_s.downcase || s.to_s.downcase) || stroke1 == nil
+        raise NoSuchStrategyError if name1 == nil || stroke1 !=~ /[PRS]/ || stroke1 == nil
         def name1
           name1.to_s.capitalize
         end
         def stroke1
-          stroke1 = (r.to_s.capitalize || p.to_s.capitalize || s.to_s.capitalize)
+          stroke1 =~ /[PRS]/
         end
     end
     def move2
       move2 = [name2, stroke2]
-        raise NoSuchStrategyError if name2 == nil || stroke2 != (r.to_s.downcase || p.to_s.downcase || s.to_s.downcase) || stroke2 == nil
+        raise NoSuchStrategyError if name2 == nil || stroke2 !=~ /[PRS]/ || stroke2 == nil
         def name2
           name2.to_s.capitalize
         end
         def stroke2
-          stroke2 = (r.to_s.capitalize || p.to_s.capitalize || s.to_s.capitalize)
+          stroke2 =~ /[PRS]/
         end
     end
     case 
-      when stroke1 == (r.to_s.capitalize || p.to_s.capitalize || s.to_s.capitalize) and  stroke2 == (s.to_s.capitalize || r.to_s.capitalize || p.to_s.capitalize)
+      when stroke1 == ("R" || "P" || "S") and  stroke2 == ("S" || "R" || "P")
         name1 
-      when stroke1 == (s.to_s.capitalize || r.to_s.capitalize || p.to_s.capitalize) and  stroke2 == (r.to_s.capitalize || p.to_s.capitalize || s.to_s.capitalize)
+      when stroke1 == ("S" || "R" || "P") and  stroke2 == ("R" || "P" || "S")
         name2 
-      when stroke1 == (r.to_s.capitalize || p.to_s.capitalize || s.to_s.capitalize) and  stroke2 == (r.to_s.capitalize || p.to_s.capitalize || s.to_s.capitalize)
+      when stroke1 == ("R" || "P" || "S") and  stroke2 == ("R" || "P" || "S")
       	name1
     end
   end
@@ -40,9 +40,3 @@ class RockPaperScissors
     end 
   end
 end
-
-
-
-
-
-
